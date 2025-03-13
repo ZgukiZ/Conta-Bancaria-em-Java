fee2f0b017421fb5b73e67ce0f850f0071fda844
Gustavo Theodoro Rodrigues <129709609+ZgukiZ@users.noreply.github.com>
2025-03-13 01:29:05 -0300

    Delete ReactJS directory

--- ReactJS/Fundamentos de ReactJS/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react' //Onde Importamos as propriedades do React
-import { createRoot } from 'react-dom/client' // É o que converte o React na DOM Javascript
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
